9215a40f88b6b619b526b7588ab4301cb14e83e0
AMEL BATOUCHE <amelbat7400@gmail.com>
2023-08-28 23:11:06 +0200

    Scope Assignement Completed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName(){
+   customerName = customerName.toUpperCase()
+   console.log(customerName)
+}
+var bestCustomer;
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+console.log(bestCustomer)
+
+
+
+
+function overwriteBestCustomer(){
+bestCustomer = "maybe bob"
+console.log(bestCustomer)
+}
+
+
+const leastFavoriteCustomer = "some value";
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = "another"
+  console.log(leastFavoriteCustomer)
+}
+changeLeastFavoriteCustomer()
+
+
